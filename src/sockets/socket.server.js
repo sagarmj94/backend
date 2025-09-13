42681faa5aa6ccbd60451762fd2e8c88c6205502
Sagar Jadhav <sagarmj1502@gmail.com>
2025-09-13 13:13:56 +0530

    Add cookie dependency and implement authentication middleware in socket server

--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -1,6 +1,18 @@
 const { Server } = require("socket.io");
+const cookie = require("cookie");
+
 function initSocketServer(httpServer) {
     const io = new Server(httpServer, {});
+
+    //middleware for authentication
+    io.use((socket, next) => {
+        const cookiesData = cookie.parse(socket.handshake.headers.cookie);
+        if (!cookiesData.token) {
+            return next(new Error("Authentication error"));
+        }
+        socket.token = cookiesData.token;
+        next();
+    });
 
     io.on("connection", (socket) => {
         console.log("a user connected", socket.id);
